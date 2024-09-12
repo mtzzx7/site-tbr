743fc4c7ed1b573d29b537f8311f2b77b9665417
Matheus <154036033+mtzzx7@users.noreply.github.com>
2024-09-12 08:18:31 -0300

    Add files via upload

--- site/lightmode.js
+++ site/lightmode.js
@@ -72,3 +72,26 @@ function checkChar2(e) {
         return true;
     }
 }
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelector('section');
+let navLinks = document.querySelector('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height){
+            navLinks.forEac(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add ('active');
+            })
+        }
+    })
+}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
